cd4634efdcadff90758ce683a56b08db1689759a
JasonQSY <jasonsyqian@gmail.com>
2017-04-17 21:16:37 +0800

    add git push.default

--- git.sh
+++ git.sh
@@ -8,5 +8,6 @@ sudo apt-get install git -y
 # git config
 git config --global user.name "JasonQSY"
 git config --global user.email "jasonsyqian@gmail.com"
+git config --global push.default simple
 
 echo "[Success] git installed and configured."
